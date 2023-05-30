a8ccef1d6aabfce0c1d7123ee307cd58ac3be6b7
TendaniMamadi <mamadi.tendani@gmail.com>
2023-05-30 13:17:17 +0200

    my first commit

--- electricity.dom.js
+++ electricity.dom.js
@@ -1,9 +1,28 @@
 // DOM element(s) references
 
+var radioElement = document.querySelector(".topup");
+var appliancesElement = document.querySelector(".usage");
+var buyingButton = document.querySelector(".Btn");
+var useButton = document.querySelector(".useNow");
+
+var totalUnitsAvailable = document.querySelector(".unitsAvailable");
+var unitsBoughtElem = document.querySelector(".totalUnits");
+var amountSpentElem = document.querySelector(".totalAmount");
+var advanceCreditElem = document.querySelector(".advanceTaken");
 
 
 // Factory Function instance 
-const electricity =  Electricity();
+const electricity = Electricity();
 
 
 // DOM events here 
+
+buyingButton.addEventListener("click", function () {
+
+    alert('clicked')
+});
+
+useButton.addEventListener("click", function () {
+
+    alert('clicked')
+});
--- electricity.js
+++ electricity.js
@@ -8,12 +8,24 @@ function Electricity() {
         'Fridge': 13
     };
 
+    var unitsAvailable = 0;
+    
     function topUpElectricity(amount) {
-
+        //units bought are equal to amount
+        if(amount == 10){
+            unitsAvailable+=7
+        }
+        else if(amount == 20){
+            unitsAvailable+=14
+        }
+        else if(amount == 50){
+            unitsAvailable+=35
+        }
 
     }
 
     function getUnitsAvailable() {
+
          return unitsAvailable;
     }
 
@@ -22,13 +34,31 @@ function Electricity() {
     * other wise return false and do nothing.
     */
     function useAppliance(appliance) {
-        
+
+        //each appliance has units it consumes, i want the value of each to be subtracted from units available
+      if(appliance === "Stove"){
+        unitsAvailable - appliances.Stove
+      }
+
+      if(appliance === "Fridge"){
+        unitsAvailable - appliances.Fridge
+      }
+
+      if(appliance === "Kettle"){
+        unitsAvailable - appliances.Kettle
+      }
+
+      if(appliances === "TV"){
+        unitsAvailable - appliances.TV
+      }
     }
 
     function advanceTaken() {
+
     }
 
     function totalAmountSpent() {
+
     }
 
     function totalUnitsBought(){
